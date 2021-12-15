23eee8d8f893bef6956b0b6b0019caccb7717651
LIN,JIAN-CHENG <94939073+Devin900904@users.noreply.github.com>
2021-12-15 15:05:24 +0800

    7

--- 0707_Imagecircle/src/main/java/tw/tcnr14/m0707/M0707.java
+++ 0707_Imagecircle/src/main/java/tw/tcnr14/m0707/M0707.java
@@ -8,40 +8,37 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class M0707 extends AppCompatActivity  implements View.OnClickListener {
+public class M0707 extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView img1;
-    private ImageButton img2;
+    private ImageView img01;
+    private ImageButton img02;
     private TextView t001;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.m0707);
-        setupViewcomponent();
+        setupViewComponent();
     }
 
-    private void setupViewcomponent() {
-//        img1=(ImageView)findViewById(R.id.m0707_imgview);
-//        img2=(ImageButton)findViewById(R.id.m0707_imgbutton);
+    private void setupViewComponent() {
+        img01 = (ImageView)findViewById(R.id.m0707_imgview);
+        img02 = (ImageButton)findViewById(R.id.m0707_imgbutton);
         t001 =(TextView)findViewById(R.id.m0707_t001);
 
-        img1.setOnClickListener(this);
+        img01.setOnClickListener(this);
+        img02.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-//        switch (v.getId()){
-//            case R.id.m0707_imgview:
-//               t001.setText(getText(R.string.m0707_t001)+getString(R.string.m0707_imageview));
-//
-//                break;
-//            case R.id.m0707_imgbutton:
-//                t001.setText(getText(R.string.m0707_t001)+getString(R.string.m0707_ingbutton));
-//
-//                break;
-//        }
-
+        switch (v.getId()){
+            case R.id.m0707_imgview:
+                t001.setText(getString(R.string.m0707_t001)+getString(R.string.m0707_imageview));
+                break;
+            case R.id.m0707_imgbutton:
+                t001.setText(getString(R.string.m0707_t001)+getString(R.string.m0707_ingbutton));
+                break;
+        }
     }
 }
